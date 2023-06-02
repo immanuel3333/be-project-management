09615e3a8cd07c9fbb911720668d1000fea4c219
immanuel3333 <immanuel.siburian80@gmail.com>
2023-06-02 18:47:10 +0700

    setup properties

--- src/main/java/com/group3/projectmanagementapi/ProjectManagementApiApplication.java
+++ src/main/java/com/group3/projectmanagementapi/ProjectManagementApiApplication.java
@@ -2,9 +2,24 @@ package com.group3.projectmanagementapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProjectManagementApiApplication {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry corsRegistry) {
+				corsRegistry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "DELETE", "PUT")
+						.maxAge(3600);
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementApiApplication.class, args);
