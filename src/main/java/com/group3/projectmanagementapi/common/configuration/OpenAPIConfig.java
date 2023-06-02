98cc799be28b2453a23dc388a162dd2190eba70b
immanuel3333 <immanuel.siburian80@gmail.com>
2023-06-02 18:58:22 +0700

    new setup

--- src/main/java/com/group3/projectmanagementapi/common/configuration/OpenAPIConfig.java
+++ src/main/java/com/group3/projectmanagementapi/common/configuration/OpenAPIConfig.java
@@ -6,9 +6,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Simple API", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "Simple API", version = "v1"), servers = @Server(url = "/", description = "Default Server URL"))
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class OpenAPIConfig {
 
