031d83224ac8518ede8f63fb120e0466d95280da
Immanuel Siburian <immanuel.siburian80@gmail.com>
2023-06-05 10:30:02 +0700

    add validation in request card

--- src/main/java/com/group3/projectmanagementapi/card/model/dto/CardRequest.java
+++ src/main/java/com/group3/projectmanagementapi/card/model/dto/CardRequest.java
@@ -5,14 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CardRequest {
+    @NotEmpty(message = "Title is required")
     private String title;
+
+    @NotEmpty(message = "Description is required")
     private String description;
+
     private Long statusId;
 
     public Card convertToEntity() {
